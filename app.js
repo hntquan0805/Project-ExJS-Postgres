7c207ef8bbafcf83430adc96cba6bfda6901a214
Ho Ngoc Trung Quan <hntqun0805@gmail.com>
2024-11-06 11:06:55 +0700

    2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/products', productRoutes);
 app.use('/users', userRoutes);
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
