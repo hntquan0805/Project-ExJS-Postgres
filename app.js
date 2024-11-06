e9a470db42e4355e3e798c991201742f98ff1d28
hongphuc <phuc21744@gmail.com>
2024-11-06 21:16:03 +0700

    Add homepage

--- app.js
+++ app.js
@@ -11,13 +11,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.render('home');
+});
+
 app.use('/products', productRoutes);
 app.use('/users', userRoutes);
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.send('<a href="/products">Go to Product Page</a> | <a href="/users/register">Register</a>');
-});
+// app.get("/", (req, res) => {
+//   res.send('<a href="/products">Go to Product Page</a> | <a href="/users/register">Register</a>');
+// });
 
 const PORT = process.env.PORT || 10000;
 
