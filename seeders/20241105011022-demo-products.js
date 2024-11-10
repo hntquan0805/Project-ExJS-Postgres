fb0fb44b271978bf6758b0b847e73873817cda76
hongphuc <phuc21744@gmail.com>
2024-11-10 17:01:08 +0700

    Change abit of sample data

--- seeders/20241105011022-demo-products.js
+++ seeders/20241105011022-demo-products.js
@@ -4,52 +4,52 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('products', [
       {
-        id: '1',
-        name: 'Chicken Burger',
-        price: 115,
-        description: 'Ipsum ipsum clita erat amet dolor justo diam'
+      id: '1',
+      name: 'Apple',
+      price: 1.99,
+      description: 'Fresh and juicy apple'
       },
       {
-        id: '2',
-        name: 'Chicken Burger',
-        price: 115,
-        description: 'Ipsum ipsum clita erat amet dolor justo diam'
+      id: '2',
+      name: 'Banana',
+      price: 0.99,
+      description: 'Sweet and nutritious banana'
       },
       {
-        id: '3',
-        name: 'Chicken Burger',
-        price: 115,
-        description: 'Ipsum ipsum clita erat amet dolor justo diam'
+      id: '3',
+      name: 'Orange',
+      price: 1.49,
+      description: 'Citrusy and refreshing orange'
       },
       {
-        id: '4',
-        name: 'Chicken Burger',
-        price: 115,
-        description: 'Ipsum ipsum clita erat amet dolor justo diam'
+      id: '4',
+      name: 'Grapes',
+      price: 2.99,
+      description: 'Plump and delicious grapes'
       },
       {
-        id: '5',
-        name: 'Chicken Burger',
-        price: 115,
-        description: 'Ipsum ipsum clita erat amet dolor justo diam'
+      id: '5',
+      name: 'Watermelon',
+      price: 4.99,
+      description: 'Juicy and hydrating watermelon'
       },
       {
-        id: '6',
-        name: 'Chicken Burger',
-        price: 115,
-        description: 'Ipsum ipsum clita erat amet dolor justo diam'
+      id: '6',
+      name: 'Strawberries',
+      price: 3.49,
+      description: 'Sweet and tangy strawberries'
       },
       {
-        id: '7',
-        name: 'Chicken Burger',
-        price: 115,
-        description: 'Ipsum ipsum clita erat amet dolor justo diam'
+      id: '7',
+      name: 'Pineapple',
+      price: 2.99,
+      description: 'Tropical and refreshing pineapple'
       },
       {
-        id: '8',
-        name: 'Chicken Burger',
-        price: 115,
-        description: 'Ipsum ipsum clita erat amet dolor justo diam'
+      id: '8',
+      name: 'Mango',
+      price: 2.49,
+      description: 'Exotic and flavorful mango'
       }
     ], {});
   },
