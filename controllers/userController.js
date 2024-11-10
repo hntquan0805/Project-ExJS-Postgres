faa728f8bcd70ecf38a39fd87cb386d0d5a9dfa9
nnson1209 <nnson120924@gmail.com>
2024-11-10 15:01:15 +0700

    update registration function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,35 @@
 const { User } = require('../models');
 
-// exports.showRegistrationForm = (req, res) => {
-//   res.render('users/signup');
-// };
-
 exports.registerUser = async (req, res) => {
+  console.log(req.body);
   try {
-    const { name, email, password } = req.body;
+    const { name, email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).render('users/signup', {
+          title: "Sign Up",
+          errors: { confirmPassword: 'Password confirmation does not match' },
+          values: { name, email }
+      });
+    }
 
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
-      return res.status(400).send('Email already in use');
+      return res.status(400).render('users/signup', {
+        title: "Sign Up",
+        errors: { email: 'Email already in use' },
+        values: { name, email }
+      });
     }
 
     const newUser = await User.create({ name, email, password });
-    res.render('users/success', {title : "Sign Up Success", user: newUser });
+    res.render('users/success', { title: "Sign Up Success", user: newUser });
   } catch (error) {
     console.error('Error registering user:', error);
-    res.status(500).send('Error registering user');
+    res.status(500).render('users/signup', {
+      title: "Sign Up",
+      errors: { general: 'Error registering user. Please try again later.' },
+      values: { name, email }
+    });
   }
 };
-
-// exports.getAllUsers = async (req, res) => {
-//   const users = await User.findAll();
-//   res.render('users/index', { users });
-// };
