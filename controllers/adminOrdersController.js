e687fa12403cc8f61f8bfd7cb2cbcf332e31f02a
nnson1209 <nnson120924@gmail.com>
2025-01-14 15:54:00 +0700

    Filter order by status

--- controllers/adminOrdersController.js
+++ controllers/adminOrdersController.js
@@ -4,13 +4,16 @@ const { Order, OrderItem, product, User } = require('../models');
 
 exports.getAdminOrders = async (req, res) => {
     try {
-        const page = parseInt(req.query.page) || 1;
+        const { page = 1, status } = req.query;
         const limit = parseInt(req.query.limit) || 5;
         const offset = (page - 1) * limit;
-    
+        
+        const whereCondition = status ? { status } : {};
+
         const totalOrders = await Order.count();
     
         const orders = await Order.findAll({
+            where: whereCondition,
           include: [
             {
               model: OrderItem,
@@ -60,61 +63,3 @@ exports.updateOrderStatus = async (req, res) => {
     res.status(500).json({ message: 'Failed to update order status.' });
   }
 };
-
-// const orderService = require('../services/orderService');
-
-// exports.createOrder = async (req, res) => {
-//   try {
-//     const order = await orderService.createOrder(req.body);
-//     res.status(200).json({ message: 'Checkout successful', order });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-// exports.getOrderHistory = async (req, res) => {
-//   try {
-//     const userId = req.user.id;
-//     const page = parseInt(req.query.page) || 1;
-//     const limit = parseInt(req.query.limit) || 5;
-//     const offset = (page - 1) * limit;
-
-//     const totalOrders = await Order.count({ where: { userId } });
-
-//     const orders = await Order.findAll({
-//       where: { userId },
-//       include: [
-//         {
-//           model: OrderItem,
-//           as: 'orderItems',
-//           include: [
-//             {
-//               model: Product,
-//               as: 'product',
-//             },
-//           ],
-//         },
-//       ],
-//       order: [['createdAt', 'DESC']],
-//       limit: limit,
-//       offset: offset,
-//     });
-
-//     const totalPages = Math.ceil(totalOrders / limit);
-
-//     if (req.xhr) {
-//       return res.json({ orders, totalPages, currentPage: page });
-//     }
-
-//     res.render('pages/users/order_history', {
-//       title: 'Order History',
-//       orders,
-//       currentPage: page,
-//       totalPages,
-//     });
-//   } catch (err) {
-//     console.error('Error fetching order history:', err);
-//     res.render('pages/error', { title: 'Error', message: 'Failed to fetch order history.' });
-//   }
-// };
